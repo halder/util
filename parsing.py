6d68ebc469b58580da9fca7a1cba62b44f52930b
halder <sascha.halder91@gmail.com>
2022-02-17 17:31:38 +0100

    Init

---
+++ parsing.py
@@ -0,0 +1,18 @@
+# parse sql query into string to be used within pySpark code
+# makes for easier maintainability of large sql queries
+
+def parse_sql(sql_file, replacement=None):
+    """
+    sql_file: string; relative location of .sql file e.g. "../my_query.sql"
+    replacement: dict; mapping for f-string like replacement. Use curly braces within sql query to indicate variable (e.g. {date_dummy})
+    """
+
+    with open(sql_file, mode="r") as file:
+        query = file.read()
+    file.close()
+
+    if replacement:
+        for dummy, repl in replacement.items()
+            query = query.replace(dummy, repl)
+
+    return query
